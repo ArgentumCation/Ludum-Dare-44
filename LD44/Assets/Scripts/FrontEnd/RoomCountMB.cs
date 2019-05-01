1a1f633de3ee92c91898b91f1ee0c139fcda3499
RShields <playnow254@gmail.com>
2019-04-30 20:26:45 -0700

    Room counter

---
+++ LD44/Assets/Scripts/FrontEnd/RoomCountMB.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class RoomCountMB : MonoBehaviour
+{
+    private TextMeshPro _text;
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshPro>();
+    }
+
+    private void Update()
+    {
+        _text.text = "Room: " + RoomGenerator.TotalRooms;
+    }
+}
